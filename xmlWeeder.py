6915ed82cdb84a15d5b8d98934682f88829d1852
PTF-FRI <rdr006@gmail.com>
2017-04-11 12:34:42 -0500

    Update xmlWeeder.py
    Expanded this a little bit.

--- xmlWeeder.py
+++ xmlWeeder.py
@@ -1,12 +1,20 @@
 #Very rudimentary! I will work on it over the weekend
 
 import xml.etree.ElementTree as ET
+import numpy as np
 import sys
+import glob
 
-for (filename,i) in enumerate(dir(data)):
-	numObs = ET.parse(filename).getroot()[0][0][41][0]
+datafiles = glob("data/*.xml")
+for file in datafiles:
+	numObs.append = ET.parse(file).getroot()[0][0][41][0]
 #Above in that big list is where the number of observations is stored as an XML element
-	if (numObs > 4):
-		#keep
-	else
-		#destroy
+
+avgObs = np.mean(numObs)
+cutoff = avgObs - (1.5*np.std(numObs))
+print("There are " + len(numObs) + "data files here. The mean number of observations is " + avgObs +" .")
+print("Every file with less than " + cutoff + " observations will be removed.")
+#for file in datafiles:
+#	if (numObs < cutoff):
+#		os.remove(file)
+
